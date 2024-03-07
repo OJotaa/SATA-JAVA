23b4d8c2b8a69605109b1c968234d0fb2fc42352
Jota 愛 <joaovitinhoxd7@gmail.com>
2024-03-07 16:55:56 -0300

    Add files via upload

---
+++ operadores.java
@@ -0,0 +1,122 @@
+public class operadores {
+
+    /*public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }*/
+ 
+    /*public static void main(String[] args) {
+        int numero = 5;
+
+        //x numero  = - numero;
+
+        System.out.println( - numero);
+
+        //x numero = numero * -1;
+
+        System.out.println(numero);
+
+    }/* */
+
+    /*public static void main(String[] args) {
+        int numero = 5;
+
+        //x repeticao
+        numero = numero + 2;
+
+        System.out.println(numero ++);
+        System.out.println(numero);
+
+    } */
+    
+    /*public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel; //anula ela 
+
+        System.out.println(!variavel);
+
+
+    }*/
+
+    /*public static void main(String[] args) {
+        
+        int a, b;
+        
+        a = 5;
+        b = 6;
+
+        //Exemplo de condicional utilizando uma estrutura IF/ELSE
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+
+    }*/
+
+
+    /*public static void main(String[] args) {
+
+        String nomeUm = "Joao";
+        String nomeDois = new String( "Victor");
+
+        System.out.println(nomeUm == nomeDois);
+
+        int numero1 = 1;
+        int numero2 = 2;
+        
+         boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+                System.out.println("a nossa condição é verdadeira");
+            } 
+
+
+
+        System.out.println("numeroUm é igual a numeroDois??");
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois??");
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois?");
+    }*/
+
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+        
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+    }
+
+}   
+
